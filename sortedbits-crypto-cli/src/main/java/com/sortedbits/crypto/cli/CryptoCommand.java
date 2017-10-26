416d93879836911dd4144d70ec6bfb152de3cbf1
sortedibts <sortedibts@zoho.com>
2017-10-26 12:59:23 +0200

    second commit

--- sortedbits-crypto-cli/src/main/java/com/sortedbits/crypto/cli/CryptoCommand.java
+++ sortedbits-crypto-cli/src/main/java/com/sortedbits/crypto/cli/CryptoCommand.java
@@ -1,35 +1,33 @@
 package com.sortedbits.crypto.cli;
 
-import com.sortedbits.crypto.api.CryptoException;
 import com.sortedbits.crypto.api.PasswordEncryption;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 import java.util.ServiceLoader;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import static java.lang.String.format;
 import static java.lang.System.out;
 import static java.util.ServiceLoader.load;
-import static java.util.stream.StreamSupport.stream;
-
-import java.util.List;
-
-import static java.util.stream.IntStream.range;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
 
-public class Main {
+public class CryptoCommand {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         ServiceLoader<PasswordEncryption> loader = load(PasswordEncryption.class);
+
         List<ServiceLoader.Provider<PasswordEncryption>> providers = loader.stream().collect(toList());
         range(0, providers.size()).forEach(i -> out.println(format("%d - %s", i, providers.get(i).type().getName())));
 
-        out.println("text:");
-        String text = System.console().readLine();
 
-        PasswordEncryption pbe = providers.get(0).get();
-//        pbe.encrypt(null, "foo");
+            String line = new BufferedReader(new InputStreamReader(System.in)).readLine();
+
+        //PasswordEncryption pbe = providers.get(0).get();
+        //pbe.encrypt(null, "foo");
 
     }
 }
